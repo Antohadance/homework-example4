107090af22751dd137cca6d8d0c0ef82be48909c
Semec <Semechkov98@gmail.com>
2022-02-06 03:29:18 +0300

    Домашнее задание выполнено

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //Задание №1
+        for (int i = 10; i >= 1; i--) {
+            System.out.print(" " + i);
+        }
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(" " + i);
+            i++;
+        }
+        //Задание №2
+        for (int friday = 7; friday < 31; friday = friday + 7)
+            System.out.print("\n Сегодня пятница " + friday + "-е число.Необходимо подготовить отчет.");
+        //Задание №3
+        int year = 1896;
+        int year1 = year + 200;
+        for (int y2 = 1896; y2 < year1; y2 = y2 + 79) {
+            System.out.print(" \n " + y2);
+        }
     }
 }
